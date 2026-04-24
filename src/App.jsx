336a226f6ed47cf293ef072f1421c05107ae81e9
hay-ninja <hayredstonemaster@gmail.com>
2026-04-23 18:42:41 -0700

    Aha! added my own spin to the onclick behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-import * as React from 'react'
-import * as ReactBootstrap from 'react-bootstrap'
+// import * as React from 'react'
+// import * as ReactBootstrap from 'react-bootstrap'
+import { useState } from 'react'
 
-function Square({value}) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log('clicked!');
+    setValue('X');
+  }
+
+  return (
+    <button 
+      className="square"
+      onClick={() => handleClick()}
+    >
+     {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
     <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
     </>
   )
 }
